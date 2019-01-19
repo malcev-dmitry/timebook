218e8a592408d794cbc721de5e67bdd1bdd55aa7
malcev-dmitry <maloycobcem@gmail.com>
2019-01-20 00:10:27 +0300

    set up material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule, MatIconModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [BrowserModule,
+      FormsModule,
+      MatButtonModule,
+      MatIconModule,
+      MatTableModule,
+      MatPaginatorModule,
+      BrowserAnimationsModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
